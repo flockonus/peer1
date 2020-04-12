6846e9b55c8b868bf530bd40378ffe2699266012
flockonus <fabianosoriani@gmail.com>
2020-04-11 20:05:38 -0700

    compiled site

---
+++ docs/precache-manifest.7bd2d5af004277bbf55027c13403308e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa706380ea439b7838af2b362547080f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8512057bfe9f11b30a0b",
+    "url": "/static/css/main.87c0d177.chunk.css"
+  },
+  {
+    "revision": "e86433ca4038b364d0bf",
+    "url": "/static/js/2.735ae3fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.735ae3fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8512057bfe9f11b30a0b",
+    "url": "/static/js/main.8d31437d.chunk.js"
+  },
+  {
+    "revision": "27150f99074edb62bc3d",
+    "url": "/static/js/runtime-main.aa35321c.js"
+  }
+]);
